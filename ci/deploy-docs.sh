0b39a81fe5e7f28c341e0a83e214e259ab19028b
Leonardo Uieda <leouieda@gmail.com>
2018-04-03 15:26:06 -1000

    Update CI scripts and config files

--- ci/deploy-docs.sh
+++ ci/deploy-docs.sh
@@ -2,51 +2,70 @@
 #
 # Push the built HTML pages to the gh-pages branch.
 
+# Keeps pages for releases in separate folders. Pages for the master branch are
+# in the 'dev' folder. 'latest' is a link to the last release.
+
 # To return a failure if any commands inside fail
 set -e
 
 REPO=deeplook
-USER=fatiando
+USER=opengeophysics
 BRANCH=gh-pages
 CLONE_ARGS="--quiet --branch=$BRANCH --single-branch"
 REPO_URL=https://${GH_TOKEN}@github.com/${USER}/${REPO}.git
 CLONE_DIR=deploy
+# Place the HTML is different folders for different versions
+if [[ "${TRAVIS_TAG}" != "" ]]; then
+    VERSION=${TRAVIS_TAG}
+else
+    VERSION=dev
+fi
 
-echo -e "Preparing to push HTML to branch ${BRANCH} of ${USER}/${REPO}"
-
-echo -e "Copying generated files."
-cp -R doc/_build/html/ $HOME/keep
-
-# Go to home and setup git
-cd $HOME
-
-git config --global user.email "travis@nothing.com"
-git config --global user.name "TravisCI"
+echo -e "DEPLOYING HTML TO GITHUB PAGES:"
+echo -e "Target: branch ${BRANCH} of ${USER}/${REPO}"
+echo -e "HTML dir: ${VERSION}"
 
 # Clone the project, using the secret token.
 # Uses /dev/null to avoid leaking decrypted key.
 echo -e "Cloning ${USER}/${REPO}"
-git clone ${CLONE_ARGS} ${REPO_URL} $CLONE_DIR 2>&1 >/dev/null
+git clone ${CLONE_ARGS} ${REPO_URL} ${CLONE_DIR} 2>&1 >/dev/null
 
-cd $CLONE_DIR
+cd ${CLONE_DIR}
 
-# Move the old branch out of the way and create a new one:
-echo -e "Create an empty ${BRANCH} branch"
-git checkout ${BRANCH}
-git branch -m ${BRANCH}-old
-git checkout --orphan ${BRANCH}
+# Configure git to a dummy Travis user
+git config user.email "travis@nothing.com"
+git config user.name "TravisCI"
 
-# Delete all the files and replace with our good set
+# Delete all the files and replace with our new  set
 echo -e "Remove old files from previous builds"
-git rm -rf .
-cp -Rf $HOME/keep/. $HOME/$CLONE_DIR
+rm -rf ${VERSION}
+cp -Rf ${TRAVIS_BUILD_DIR}/doc/_build/html/ ${VERSION}/
 
-# add, commit, and push files
+# Need to have this file so that Github doesn't try to run Jekyll
+touch .nojekyll
+
+if [[ "${VERSION}" != "dev" ]]; then
+    echo -e "Setup link from ${VERSION} to 'latest'"
+    rm -f latest
+    ln -sf ${VERSION} latest
+fi
+
 echo -e "Add and commit changes"
-git add -f .
-git commit -m "Push from Travis build $TRAVIS_BUILD_NUMBER"
+git add -A .
+git status
+if [[ "${VERSION}" == "dev" && `git log -1 --format='%s'` == *"dev"* ]]; then
+    # If this is a dev build and the last commit was from a dev build, reuse
+    # the same commit
+    echo -e "Amending last commit"
+    git commit --amend --reset-author --no-edit
+else
+    # Make a new commit
+    echo -e "Making a new commit"
+    git commit -m "Deploy $VERSION from TravisCI"
+fi
+
 echo -e "Pushing..."
-git push -fq origin ${BRANCH} 2>&1 >/dev/null
+git push -fq origin $BRANCH 2>&1 >/dev/null
 
 echo -e "Finished uploading generated files."
 
